55e1dde3f685e586a4f87ac8a34eaa9b367eb989
Ryan Rosario <ryanrosario@comcast.net>
2022-07-08 11:32:50 -0400

    Updated Forest to use menu

--- Location/Forest/Forest.cs
+++ Location/Forest/Forest.cs
@@ -3,23 +3,33 @@ public class Forest : ILocation
     public readonly static Forest Instance = new Forest();
     public string Description => "A dense woodline surrounds the cottage in an endless ocean of bark and leaves.";
 
-    public Menu menu;
+    public Menu LocationMenu { get; }
     public string Name => "Forest";
     //public bool isTunnelDiscovered = false;
     private List<string> options = new List<string>();
 
     public Forest()
     {
-        menu = new Menu();
+        LocationMenu = new Menu();
         MenuItem exit = new MenuItem("Enter Cottage", EnterCottage);
-        menu.AddItem(exit);
+        MenuItem examine = new MenuItem("Examine", Examine);
+        MenuItem leaveforest = new MenuItem("Leave Forest", LeaveForest);
+        MenuItem entertunnel = new MenuItem("Enter Tunnel", EnterTunnel);
+        MenuItem examinetunnel = new MenuItem("Examine Tunnel", ExamineTunnel);
+        MenuItem shoutintotunnel = new MenuItem("Shout into the tunnel", ShoutIntoTunnel);
+        LocationMenu.AddItem(exit);
+        LocationMenu.AddItem(examine);
+        LocationMenu.AddItem(leaveforest);
+        LocationMenu.AddItem(entertunnel);
+        LocationMenu.AddItem(examinetunnel);
+        LocationMenu.AddItem(shoutintotunnel);
 
     }
 
-     public List<string> GetOptions()
-     {
-         return options;
-     }
+    public List<string> GetOptions()
+    {
+        return options;
+    }
 
     private void EnterCottage()
     {
@@ -27,35 +37,33 @@ public class Forest : ILocation
         GameState.Instance.SetLocation(Cottage.Instance);
         // TODO: Connect Cottage
     }
-    public void HandleInput(int option, GameState gs)
+    private void Examine()
     {
-        switch (option) {
-            case 1:
-                Narrator.WriteLine("You turn away from the forest and enter the cottage.");
-                gs.SetLocation(Cottage.Instance);
-                // TODO: Connect Cottage
-                break;
-            case 2:
-                Narrator.WriteLine("The forest seems to go on forever yet you can't hear any birds singing or animals moving within.");
-                Narrator.WriteLine("As you search the forest you find a overgrown tunnel leading into the darkness and get a strange sense of deja vu...");
-                break;
-            case 3:
-                Narrator.WriteLine("You walk deeper into the forest, trying to find a way out. After a few minutes of traveling you come across a small only to realize you've travelled in a circle.");
-                break;
-            case 4:
-                //if => you don't a weapon Narrator.WriteLine("You feel a grave sense of danger lurking within the tunnel and wonder if it is truly safe to enter...");
-                //else...
-                Narrator.WriteLine("You enter the tunnel");
-                gs.SetLocation(TunnelEntrance.Instance);
-                break;
-            case 5:
-                Narrator.WriteLine("The tunnel seems like it was abandoned along time ago. A faint, warm wind caresses your face from its mouth.");
-                break;
-            case 6:
-                Narrator.WriteLine("You shout into the tunnel and can hear your voice echo back to you before sharply cutting off.");
-                break;
-            default:
-                break;
-        }
+        Narrator.WriteLine("The forest seems to go on forever yet you can't hear any birds singing or animals moving within.");
+        Narrator.WriteLine("As you search the forest you find a overgrown tunnel leading into the darkness and get a strange sense of deja vu...");
+        GameState.Instance.SetLocation(Cottage.Instance);
+        // TODO: Connect Cottage
+    }
+    private void LeaveForest()
+    {
+        Narrator.WriteLine(@"You walk deeper into the forest, trying to find a way out.
+        After a few minutes of traveling you come across a small only to realize you've travelled in a circle.");
+    }
+    private void EnterTunnel()
+    {
+        Narrator.WriteLine("You enter the tunnel");
+        GameState.Instance.SetLocation(TunnelEntrance.Instance);
+    }
+    private void ExamineTunnel()
+    {
+        Narrator.WriteLine("The tunnel seems like it was abandoned along time ago. A faint, warm wind caresses your face from its mouth.");
+
+    }
+
+    private void ShoutIntoTunnel()
+    {
+        Narrator.WriteLine("You shout into the tunnel and can hear your voice echo back to you before sharply cutting off.");
+
     }
 }
+    
