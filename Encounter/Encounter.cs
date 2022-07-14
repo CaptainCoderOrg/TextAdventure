2579a37c231dff947388cd952f48b12f6b047e90
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-14 08:40:47 -0700

    wip: Combat menu

--- Encounter/Encounter.cs
+++ Encounter/Encounter.cs
@@ -2,7 +2,7 @@ using System.IO;
 public class Encounter
 {
     private readonly List<LivingEntity> _enemies = new();
-    private readonly List<Player> _players = new ();
+    private readonly List<Player> _players = new();
     private readonly string _screenTemplate;
     private readonly string _rowTemplate;
 
@@ -15,7 +15,7 @@ public class Encounter
             if (typeof(Player).IsInstanceOfType(e))
             {
                 this._players.Add((Player)e);
-            } 
+            }
             else
             {
                 this._enemies.Add(e);
@@ -28,16 +28,35 @@ public class Encounter
         Console.Clear();
         string screen = _screenTemplate;
         string rows = "";
-        
-        foreach (LivingEntity entity in _enemies)
-        {
+        LivingEntity currEntity;
+        Player currPlayer;
+
+
+        for(int i = 0; i < _enemies.Count(); i++) {
+            currEntity = _enemies[i];
+            currPlayer = _players[i];
             string row = _rowTemplate;
-            string name = entity.Name.Length <= 13 ? entity.Name.PadRight(13) : entity.Name.PadRight(13).Substring(0, 10) + "...";
-            row = row.Replace("{NAME}".PadRight(13), name);
-            row = row.Replace("{HP}".PadRight(3), entity.Hp.ToString().PadLeft(3));
-            row = row.Replace("{MAX_HP}".PadRight(3), entity.MaxHp.ToString().PadLeft(3));
+            string enemyName = currEntity.Name.Length <= 13 ? currEntity.Name.PadRight(13) : currEntity.Name.PadRight(13).Substring(0, 10) + "...";
+            row = row.Replace("{NAME}".PadRight(13), enemyName);
+            row = row.Replace("{HP}".PadRight(3), currEntity.Hp.ToString().PadLeft(3));
+            row = row.Replace("{MAX_HP}".PadRight(3), currEntity.MaxHp.ToString().PadLeft(3));
+            row = row.Replace("{Player Name}", currPlayer.Name.ToString());
+            row = row.Replace("{pHP}".PadRight(3), currPlayer.Hp.ToString().PadLeft(3));
+            row = row.Replace("{pMAX_HP}".PadRight(3), currPlayer.MaxHp.ToString().PadLeft(3));
             rows += row + "\n";
         }
+
+        // foreach (LivingEntity entity in _enemies)
+        // {
+        //     string row = _rowTemplate;
+        //     string name = entity.Name.Length <= 13 ? entity.Name.PadRight(13) : entity.Name.PadRight(13).Substring(0, 10) + "...";
+        //     row = row.Replace("{NAME}".PadRight(13), name);
+        //     row = row.Replace("{HP}".PadRight(3), entity.Hp.ToString().PadLeft(3));
+        //     row = row.Replace("{MAX_HP}".PadRight(3), entity.MaxHp.ToString().PadLeft(3));
+        //     row = row.Replace("{Player Name}", _players[0].Name.ToString());
+
+        //     rows += row + "\n";
+        // }
         screen = screen.Replace("{COMBAT_ROWS}\n", rows);
         Console.WriteLine(screen);
     }
